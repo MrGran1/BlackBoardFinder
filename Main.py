cfa0e46eeb948b4a64ffb4390d801bb3c9e1f973
MrGran1 <tigran.wattrelos@gmail.com>
2023-02-21 12:04:28 +0100

    delimitation

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.image as pltimg
+import matplotlib.pyplot as plt
 
 def comparerPixel(img,pixel1,pixel2):
     """
@@ -8,13 +10,14 @@ def comparerPixel(img,pixel1,pixel2):
     pixel1/pixel2 : tuple (x,y) representant les coordoonée d'un pixel
     return boolean
     """
+    
     x1,y1 = pixel1
     x2,y2=pixel2
-    intensité = 0
+    intensite = 0
     for i in range(3):
-        intensite += abs(img[x1][y1][i]-img[x2][y2][i])
+        intensite += abs(img[y1-1][x1-1][i]-img[y2-1][x2-1][i])
 
-    if (intensité<50):
+    if (intensite<50):
         return True
 
     else:
@@ -33,3 +36,34 @@ def griserPixel(img,pixel):
     for i in range(3):
         img[x][y][i] = 0.5*img[x][y][i] 
 
+
+def delimitationImage (img):
+
+    """ Creer un numpy array qui correspond à une image qui explicite les différentes partie d'une image
+    param : 
+    img : numpy array qui represente une image
+    """
+    x,y = len(img[0]),len(img)
+
+    print (x,y)
+    imgFinale = np.zeros((y,x))
+
+
+    for j in range (y):
+        for i in range (x):
+            if(j != y-1):
+                if comparerPixel(img,(i,j),(i,j+1)):
+                    imgFinale [j+1,i] = 255
+
+            if (i != x-1):
+                if comparerPixel(img,(i,j),(i+1,j)):
+                    imgFinale [j,i+1] = 255
+
+
+    return imgFinale
+
+img = pltimg.imread("./17.jpg")
+
+finale = delimitationImage(img)
+plt.imshow(finale,cmap ='gray')
+plt.show()
