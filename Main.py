9dad9dbedc5a14ecf3734100cb42cd02777fb25f
MrGran1 <106485344+MrGran1@users.noreply.github.com>
2023-05-10 14:48:49 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.image as pltimg
 import matplotlib.pyplot as plt
 
+from collections import deque
+
 
 def comparerPixel(img,pixel1,pixel2):
     """
@@ -192,82 +194,145 @@ def taux_reussite(img, vt):
 
 
 
-def connexite4(img) : 
-    """listePixel : une liste contenat les pixels déjà visité
-        coordonnée : (i,j) du pixel
-    img ; numpy array de l'image
-    imgFinale : numpy array d'une image noire
+# def connexite4(img) : 
+#     """listePixel : une liste contenat les pixels déjà visité
+#         coordonnée : (i,j) du pixel
+#     img ; numpy array de l'image
+#     imgFinale : numpy array d'une image noire
 
-    """
-    coordonnee = (int(img.shape[1]/2),int(img.shape[0]/2))
-    listeCoordonnee = [coordonnee]
-    imgFinale = np.zeros((img.shape[1],img.shape[0]))
-    visite = []
+#     """
+#     coordonnee = (int(img.shape[1]/2),int(img.shape[0]/2))
+#     listeCoordonnee = [coordonnee]
+#     imgFinale = np.zeros((img.shape[1],img.shape[0]))
+#     visite = []
 
-    while len(listeCoordonnee) != 0:
-        i,j = coordonnee
-       # print(i,j)
-        visite.append(coordonnee)
-        listeCoordonnee.pop(0)
+#     while len(listeCoordonnee) != 0:
+#         i,j = coordonnee
+#        # print(i,j)
+#         visite.append(coordonnee)
+#         listeCoordonnee.pop(0)
 
-        if j<img.shape[0] and (i,j+1) not in visite and not img[i][j+1] == 0:
+#         if j<img.shape[0] and (i,j+1) not in visite and not img[i][j+1] == 0:
             
-            listeCoordonnee.append((i,j+1))
-            imgFinale[i][j+1] = 255
+#             listeCoordonnee.append((i,j+1))
+#             imgFinale[i][j+1] = 255
         
-        if j>0 and (i,j-1) not in visite and not img[i][j-1] == 0: 
+#         if j>0 and (i,j-1) not in visite and not img[i][j-1] == 0: 
             
-            listeCoordonnee.append((i,j-1))
-            imgFinale[i][j-1] = 255
+#             listeCoordonnee.append((i,j-1))
+#             imgFinale[i][j-1] = 255
 
-        if i<img.shape[1] and (i+1,j) not in visite and not img[i+1][j] == 0: 
+#         if i<img.shape[1] and (i+1,j) not in visite and not img[i+1][j] == 0: 
             
-            listeCoordonnee.append((i+1,j))
-            imgFinale[i+1][j] = 255
+#             listeCoordonnee.append((i+1,j))
+#             imgFinale[i+1][j] = 255
 
         
-        if i>0 and (i-1,j) not in visite and not img[i-1][j] == 0: 
+#         if i>0 and (i-1,j) not in visite and not img[i-1][j] == 0: 
             
-            listeCoordonnee.append((i-1,j))
-            imgFinale[i-1][j] = 255
+#             listeCoordonnee.append((i-1,j))
+#             imgFinale[i-1][j] = 255
 
-        if len(listeCoordonnee) != 0:
-            coordonnee = listeCoordonnee[0]
+#         if len(listeCoordonnee) != 0:
+#             coordonnee = listeCoordonnee[0]
+
+#     return imgFinale
+            
+# def connexite4(img): 
+#     """listePixel : une liste contenat les pixels déjà visité
+#         coordonnée : (i,j) du pixel
+#     img ; numpy array de l'image
+#     imgFinale : numpy array d'une image noire
+
+#     """
+#     coordonnee = (int(img.shape[1]/2),int(img.shape[0]/2))
+#     listeCoordonnee = [coordonnee]
+#     imgFinale = np.zeros((img.shape[1],img.shape[0]))
+#     visite = set()
+
+#     while listeCoordonnee:
+#         i,j = coordonnee
+#        # print(i,j)
+#         visite.add(coordonnee)
+#         listeCoordonnee.pop(0)
+
+#         if j<img.shape[0] and (i,j+1) not in visite and img[i][j+1] != 0:
+#             listeCoordonnee.append((i,j+1))
+#             imgFinale[i][j+1] = 255
+        
+#         if j>0 and (i,j-1) not in visite and img[i][j-1] != 0: 
+#             listeCoordonnee.append((i,j-1))
+#             imgFinale[i][j-1] = 255
+
+#         if i<img.shape[1] and (i+1,j) not in visite and img[i+1][j] != 0: 
+#             listeCoordonnee.append((i+1,j))
+#             imgFinale[i+1][j] = 255
+
+#         if i>0 and (i-1,j) not in visite and img[i-1][j] != 0: 
+#             listeCoordonnee.append((i-1,j))
+#             imgFinale[i-1][j] = 255
+
+#         if listeCoordonnee:
+#             coordonnee = listeCoordonnee[0]
+
+#     return imgFinale
+
+
+
+def connexite4(img):
+    h, w = img.shape[:2]
+    start = (w // 2, h // 2)
+
+    visited = set()
+    visited.add(start)
+
+    q = deque()
+    q.append(start)
+
+    imgFinale = np.zeros((h, w), dtype=np.uint8)
+
+    while q:
+        i, j = q.popleft()
+        imgFinale[j, i] = 255
+
+        for ni, nj in ((i+1, j), (i-1, j), (i, j+1), (i, j-1)):
+            if 0 <= ni < w and 0 <= nj < h and (ni, nj) not in visited and img[nj, ni] != 0:
+                visited.add((ni, nj))
+                q.append((ni, nj))
 
     return imgFinale
-            
 
 # OpenCV (Open Computer Vision) est une bibliothèque graphique. 
 # Elle est spécialisée dans le traitement d’images, que ce soit pour de la photo ou de la vidéo
-def contours(img):
+# def contours(img):
 
-    # convertie l'image couleur en une image en nuance de gris
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   
+#     # convertie l'image couleur en une image en nuance de gris
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   
     
-    # Pour réaliser une segmentation binaire avec OpenCV nous utilisons la fonction threshold()
-    # qui prend l'image originale, le seuil, la valeur maximale des pixels et l'attribut THRESH_BINARY en paramètres
-    # all pixels value above 120 will be set to 255
-    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY)  
+#     # Pour réaliser une segmentation binaire avec OpenCV nous utilisons la fonction threshold()
+#     # qui prend l'image originale, le seuil, la valeur maximale des pixels et l'attribut THRESH_BINARY en paramètres
+#     # all pixels value above 120 will be set to 255
+#     ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY)  
 
-    # Dilate les éléments blancs de l'image en fonction des 3 nombres ci dessous
-    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
-    dilation = cv2.dilate(thresh, rect_kernel, iterations = 1)  
+#     # Dilate les éléments blancs de l'image en fonction des 3 nombres ci dessous
+#     rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
+#     dilation = cv2.dilate(thresh, rect_kernel, iterations = 1)  
 
-    # C'est avec "dilation" qu'il faudra comparer la verité terrain.
-    # Le reste de cette fonction c'est juste pour bien encadrer l'image en fonction de "dilation" et avoir un meilleur affichage pour l'utilisateur
+#     # C'est avec "dilation" qu'il faudra comparer la verité terrain.
+#     # Le reste de cette fonction c'est juste pour bien encadrer l'image en fonction de "dilation" et avoir un meilleur affichage pour l'utilisateur
 
-    # Trouve les contours 
-    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+#     # Trouve les contours 
+#     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    #Dessine les contours sur l'image de base à partir de l'image dilatée
-    imgContour = cv2.drawContours(img, contours, -1, (0,255,255), 3)    
+#     #Dessine les contours sur l'image de base à partir de l'image dilatée
+#     imgContour = cv2.drawContours(img, contours, -1, (0,255,255), 3)    
 
 
-    # cv2.imshow("img",imgContour)              #cv2 a des problèmes (énoncé ci dessous) que plt permet de résoudre
-    # cv2.waitKey(0)
-    plt.imshow(imgContour, cmap = "gray")         #plt permet d'afficher l'image entiere et de la déplacer pour voir les contours 
-    # plt.imshow(imgContour)
-    plt.show()
+#     # cv2.imshow("img",imgContour)              #cv2 a des problèmes (énoncé ci dessous) que plt permet de résoudre
+#     # cv2.waitKey(0)
+#     plt.imshow(imgContour, cmap = "gray")         #plt permet d'afficher l'image entiere et de la déplacer pour voir les contours 
+#     # plt.imshow(imgContour)
+#     plt.show()
     
 
 
@@ -296,10 +361,10 @@ def comparaison_images ():
 def main():
     
     #SEGMENTATION ET BINARISATION DU TABLEAU-
-    #img = pltimg.imread("./Images_Train_et_test/Entrainement_(57)/29.jpg")
-    #contours(img)
-    #gris = convGris(img)
-    #seuilCentre(gris)
+    img = pltimg.imread(r"./Images_Train_et_test/Entrainement_(57)/77.jpg")
+    # contours(img)
+    gris = convGris(img)
+    seuilCentre(gris)
     # #-
 
     # #BINARISATION DE LA VERITE TERRAIN-
@@ -314,11 +379,14 @@ def main():
     # #-
     # imgFinale = np.zeros((gris.shape[0],gris.shape[1]),dtype = np.uint8)
     # #listecoordonnee = []
-    #img_final = connexite4(gris)
+    img_final = connexite4(gris)
 
-    #plt.imshow(img_final, cmap ='gray')
-    #plt.imshow(gris, cmap ='gray')
+    
+    plt.imshow(img_final, cmap ='gray')
+    plt.show()
+    plt.imshow(gris, cmap ='gray')
 
-    #plt.show()
-    comparaison_images()
+    plt.show()
+    #comparaison_images()
 main()
+ 
