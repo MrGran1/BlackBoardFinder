9981e516f8febe6950857a24884d44cdef508fab
Ilan <106468200+ilanouk@users.noreply.github.com>
2023-05-19 19:54:34 +0200

    resultatFinal

--- Main_Reperage/Evaluation.py
+++ Main_Reperage/Evaluation.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.image as pltimg
+import cv2
 
 from Fonctions_de_base import *
 from Reperage_Tableau import *
-from Evaluation import *
+from Reperage_Image import *
+from Seuil_Couleur import *
 
 
 
@@ -19,6 +21,25 @@ def binarisationVT_tableau(img):
             if img[i,j,0] > 0.1 or img[i,j,1] > 0.1:
                 new_img[i,j] = 255
             else :
+                new_img[i,j] = 0
+    return new_img
+
+def binarisationVT_tableauMax(img):
+    """ Binarise la VT du reperage tableau, texte et hors tableau
+    param : 
+    img : VT sous forme d'image
+    """
+    new_img = np.zeros((img.shape[0],img.shape[1]),dtype = np.uint8)
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i,j,0] > 0.1:    #Rouge -> Blanc
+                new_img[i,j] = 255
+
+            elif img[i,j,1] > 0.1:  #Vert -> Gris
+                new_img[i,j] = 128
+
+            else :                  #Sans couleur -> Noir
                 new_img[i,j] = 0
     return new_img
 
@@ -94,28 +115,65 @@ def taux_reussiteV2(img, vt):
 
     return taux_reussite
 
+def taux_reussiteV3(img, vt):
+    """
+    Compare l'image gris noir blanche, de reperage_image avec la VT et renvoie le taux de réussite
+    """
+    pixel_correct = 0
+    total_pixel = 0
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i,j] ==  255 and vt[i,j] == 255 :
+                pixel_correct +=1
+
+            elif img[i,j] ==  128 and vt[i,j] == 128 :
+                pixel_correct +=1
+
+            if img[i,j] == 255 or vt[i,j] == 255:
+                total_pixel += 1
+
+            elif img[i,j] == 125 or vt[i,j] == 128:
+                total_pixel += 1
+
+            if img[i,j] == 255 and vt[i,j] == 128:
+                pixel_correct += 0.5
+
+
+    taux_reussite = (pixel_correct / total_pixel) * 100
+
+    return taux_reussite
+
 def comparaison_images ():
     nbBon = 0
     nbTotal = 0
     tabTest = [3,9,13,18,26,28,49,51,52,61,68,69,70,79,80]
-    file = open("resultat.txt",'a')
+    file = open("resultatFinal.txt",'a')
     for i in tabTest:
         # Repérage + comparaison VT
-        print (i)
+        print(i)
         nbTotal += 1
-        gris = reperage_tab(i,"Test")
+
+        img = pltimg.imread("./Images_Train_et_test/Test_(16)/"+str(i)+".jpg")
+
+        #dilatation du texte blanc
+        elemV1 = seuilTexteBlanc(img)
+        rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 25))
+        dil = cv2.dilate(elemV1, rect_kernel, iterations = 5)
+
+        image = reperageImage(i, dil, img)
         vt_img = pltimg.imread("./Json/"+ str(i)+"VT" + "/label.png")
         print("Binarisation VT image")
-        vt_bin = binarisationVT_tableau(vt_img)
+        vt_bin = binarisationVT_tableauMax(vt_img)
         print("Taux de reussite")
-        taux = taux_reussiteV2(gris, vt_bin)
-        print ("Taux de réussite de l'image " + str(i) + ":" + str(taux))
+        taux = taux_reussiteV3(image, vt_bin)
+        print ("Taux de réussite de l'image " + str(i) + " : " + str(taux))
         file.write("Taux de réussite de l'image " + str(i) + " : " + str(taux) + "\n")
 
-        if (taux>70):
+        if (taux>60):
             nbBon +=1
     file.write("Taux de réussite finale : " + str(nbBon/nbTotal))
     file.close()
-    print("Taux de réussite : ", nbBon/nbTotal)
+    print("Taux de réussite : ", nbBon/nbTotal) 
 
 comparaison_images()
--- Main_Reperage/Reperage_Image.py
+++ Main_Reperage/Reperage_Image.py
@@ -7,8 +7,9 @@ def reperageImage(i, dilatation, img):
     """
     Fonction qui classe les éléments de l'image par rapport à Reperage_Tableau. Si l'élément est en dehors du tableau on le met en noir.
     """
-    new_img = img.copy()
-    tab = reperage_tab(i, "Train")
+    new_img = np.zeros((img.shape[0], img.shape[1]), np.uint8)
+
+    tab = reperage_tab(i, "Test")
     tab = tab.astype(np.uint8)
 
     # On récupère les dimensions de l'image
@@ -24,9 +25,11 @@ def reperageImage(i, dilatation, img):
         for x in range(w):
             if tab[int(y*hTab/h)][int(x*wTab/w)] == 0: # Si le pixel est en dehors du tableau
                 new_img[y][x] = 0
+
             elif dilatation[y][x] == 255:              # Si le pixel est dans le tableau et que c'est du texte
                 new_img[y][x] = 128
+
             else:                                      # Si le pixel est dans le tableau et que ce n'est pas du texte
                 new_img[y][x] = 255
 
-    return new_img
+    return new_img 
